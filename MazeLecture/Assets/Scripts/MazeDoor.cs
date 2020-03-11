e89ce60b63d3078b0c83399290904a70cb9592c6
Kanchuking1 <kanchuking1@gmail.com>
2020-03-11 19:01:46 +0530

    Class 1
    Download this

---
+++ MazeLecture/Assets/Scripts/MazeDoor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class MazeDoor : MazePassage
+{
+    public Transform slide;
+
+    public override void Initialize(MazeCell primary, MazeCell other, MazeDirection direction)
+    {
+        base.Initialize(primary,other,direction);
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform child = transform.GetChild(i);
+            if (child != slide)
+            {
+                child.GetComponent<Renderer>().material = cell.room.settings.wallMaterial;
+            }
+        }
+    }
+}
